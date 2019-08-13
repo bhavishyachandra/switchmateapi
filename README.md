5683e502bb768b572bae4e808bfc983d343cf9b1
bkamineni <bhavishyachandra@gmail.com>
2019-08-12 20:19:29 -0400

    updating shell script

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Instructions to run:
 
 ```console
+curl https://raw.githubusercontent.com/bhavishyachandra/switchmateapi/master/run.sh -o run.sh
 chmod u+x run.sh
 ./run.sh
 ```
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install python3.6
 sudo apt-get install git
-git pull https://github.com/bhavishyachandra/switchmateapi.git
+git clone https://github.com/bhavishyachandra/switchmateapi.git
+cd switchmateapi
 sudo pip3 install docker-compose
 sudo docker-compose up -d
